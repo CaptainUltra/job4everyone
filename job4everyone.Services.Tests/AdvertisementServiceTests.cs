970016c0509f4ea26e1628a3a50a1e0f224f8780
Stoyan Haydushki <s.haidushky@abv.bg>
2020-07-29 16:43:25 +0300

    Implelment Advertisement CRUD & tests

---
+++ job4everyone.Services.Tests/AdvertisementServiceTests.cs
@@ -0,0 +1,146 @@
+﻿using job4everyone.Data;
+using job4everyone.Models;
+using Microsoft.EntityFrameworkCore;
+using NuGet.Frameworks;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace job4everyone.Services.Tests
+{
+    class AdvertisementServiceTests
+    {
+        private DbContextOptions<Job4EveryoneDbContext> contextOptions;
+        private Job4EveryoneDbContext context;
+        private int jobPositionId;
+
+        public AdvertisementServiceTests()
+        {
+            this.contextOptions = new DbContextOptionsBuilder<Job4EveryoneDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.context = new Job4EveryoneDbContext(this.contextOptions);
+            var jobPosition = new JobPosition() { Name = "Job position" };
+            this.context.Add(jobPosition);
+            this.context.SaveChanges();
+            this.jobPositionId = jobPosition.Id;
+        }
+
+        [TearDown]
+        public void CleanUpDatabase()
+        {
+            this.context.Database.EnsureDeleted();
+            this.context = null;
+        }
+
+        [Test]
+        public void Advertisement_CanBeCreated()
+        {
+            var service = new AdvertisementService(context);
+
+            var advertisement = service.CreateAdvertisement("Advertisement1", "An advertisement for a job.", true, this.jobPositionId);
+
+            Assert.AreEqual("Advertisement1", advertisement.Name);
+            Assert.AreEqual(1, advertisement.Id);
+            Assert.AreEqual(1, context.Advertisements.Count());
+            Assert.AreEqual(jobPositionId, advertisement.JobPositionId);
+            Assert.IsNotNull(advertisement.JobPosition);
+        }
+        [Test]
+        public void AdvertisementWithInvalidJobPositionId_ThrowsException_WhenCreated()
+        {
+            var service = new AdvertisementService(context);
+
+            var ex = Assert.Throws<ArgumentException>(() => service.CreateAdvertisement("Advertisement1", "An advertisement for a job.", true, 2));
+            Assert.That(ex.Message, Is.EqualTo("Invalid job position id. (Parameter 'id')"));
+        }
+        
+        [Test]
+        public void Advertisement_CanBeRetrieved()
+        {
+            context.Advertisements.Add(new Advertisement { Name = "Advertisement1", Description = "An advertisement for a job.", Active = true, JobPositionId = this.jobPositionId });
+            context.SaveChanges();
+            var service = new AdvertisementService(context);
+
+            var advertisement = service.GetAdvertisement(1);
+
+            Assert.AreEqual(1, advertisement.Id);
+            Assert.AreEqual("Advertisement1", advertisement.Name);
+        }
+        [Test]
+        public void AdvertisementWithInvalidId_WhenRetrieved_ThrowsExeption()
+        {
+            context.Advertisements.Add(new Advertisement { Name = "Advertisement" });
+            context.SaveChanges();
+            var service = new AdvertisementService(context);
+
+            var ex = Assert.Throws<ArgumentException>(() => service.GetAdvertisement(2));
+            Assert.That(ex.Message, Is.EqualTo("Invalid advertisement id. (Parameter 'id')"));
+        }
+        [Test]
+        public void AdvertisementsList_CanBeRetrieved()
+        {
+            context.Advertisements.Add(new Advertisement { Name = "Advertisement1", Description = "An advertisement for a job.", Active = true, JobPositionId = this.jobPositionId });
+            context.Advertisements.Add(new Advertisement { Name = "Advertisement2", Description = "An advertisement for a job.", Active = true, JobPositionId = this.jobPositionId });
+            context.SaveChanges();
+            var service = new AdvertisementService(context);
+
+            var advertisements = service.GetAdvertisementsList();
+
+            Assert.AreEqual(2, advertisements.Count);
+        }
+        [Test]
+        public void Advertisement_CanBeUpdated()
+        {
+            var jobPosition = new JobPosition() { Name = "Jpos2" };
+            context.JobPositions.Add(jobPosition);
+            context.Advertisements.Add(new Advertisement { Name = "Advertisement1", Description = "An advertisement for a job.", Active = true, JobPositionId = this.jobPositionId });
+            context.SaveChanges();
+            var service = new AdvertisementService(context);
+            var updateData = new Advertisement { Name = "Advertisement2" , Active = false, JobPosition = jobPosition };
+
+            var advertisement = service.UpdateAdvertisement(1, updateData);
+            var advertisementRecord = context.Advertisements.Single(a => a.Name == "Advertisement2");
+
+            Assert.AreEqual("Advertisement2", advertisement.Name);
+            Assert.AreEqual(false, advertisement.Active);
+            Assert.AreEqual("Advertisement2", advertisementRecord.Name);
+            Assert.AreEqual(2, advertisement.JobPositionId);
+        }
+        [Test]
+        public void AdvertisementWithInvalidId_WhenUpdated_ThrowsExeption()
+        {
+            var service = new AdvertisementService(context);
+            var updateData = new Advertisement { Name = "Advertisement1", Description = "An advertisement for a job.", Active = true, JobPositionId = this.jobPositionId };
+
+            var ex = Assert.Throws<ArgumentException>(() => service.UpdateAdvertisement(1, updateData));
+            Assert.That(ex.Message, Is.EqualTo("Invalid advertisement id. (Parameter 'id')"));
+        }
+        [Test]
+        public void Advertisement_CanBeDeleted()
+        {
+            context.Advertisements.Add(new Advertisement { Name = "Advertisement1", Description = "An advertisement for a job.", Active = true, JobPositionId = this.jobPositionId });
+            context.SaveChanges();
+            var service = new AdvertisementService(context);
+
+            service.DeleteAdvertisement(1);
+
+            Assert.AreEqual(0, context.Advertisements.Count());
+        }
+        [Test]
+        public void AdvertisementWithInvalidId_WhenDeleted_ThrowsExeption()
+        {
+            var service = new AdvertisementService(context);
+
+            var ex = Assert.Throws<ArgumentException>(() => service.DeleteAdvertisement(1));
+            Assert.That(ex.Message, Is.EqualTo("Invalid advertisement id. (Parameter 'id')"));
+        }
+    }
+}
---
+++ job4everyone.Services/AdvertisementService.cs
@@ -0,0 +1,104 @@
+﻿using job4everyone.Data;
+using job4everyone.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace job4everyone.Services
+{
+    public interface IAdvertisementService
+    {
+        List<Advertisement> GetAdvertisementsList();
+        Advertisement GetAdvertisement(int id);
+        Advertisement CreateAdvertisement(string name, string description, bool active, int jobPositionId);
+        Advertisement UpdateAdvertisement(int id, Advertisement advertisement);
+        void DeleteAdvertisement(int id);
+        void ChangeAllAdvertisementsToInactive();
+        void ChangeLast10AdvertisementsToActive();
+    }
+
+    public class AdvertisementService : IAdvertisementService
+    {
+        private Job4EveryoneDbContext context;
+
+        public AdvertisementService(Job4EveryoneDbContext context)
+        {
+            this.context = context;
+        }
+        public void ChangeAllAdvertisementsToInactive()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ChangeLast10AdvertisementsToActive()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Advertisement CreateAdvertisement(string name, string description, bool active, int jobPositionId)
+        {
+            var jobPosition = this.context.JobPositions.FirstOrDefault(jp => jp.Id == jobPositionId);
+            if (jobPosition == null)
+            {
+                throw new ArgumentException("Invalid job position id.", "id");
+            }
+            //TODO: Add check for 10 active
+            var advertisement = new Advertisement() { Name = name, Description = description, Active = active};
+            advertisement.JobPosition = jobPosition;
+            this.context.Add(advertisement);
+            this.context.SaveChanges();
+
+            return advertisement;
+        }
+
+        public void DeleteAdvertisement(int id)
+        {
+            var advertisement = this.context.Advertisements.FirstOrDefault(a => a.Id == id);
+            if (advertisement == null)
+            {
+                throw new ArgumentException("Invalid advertisement id.", "id");
+            }
+            this.context.Advertisements.Remove(advertisement);
+            this.context.SaveChanges();
+        }
+
+        public Advertisement GetAdvertisement(int id)
+        {
+            var advertisement = this.context.Advertisements.FirstOrDefault(a => a.Id == id);
+            if (advertisement == null)
+            {
+                throw new ArgumentException("Invalid advertisement id.", "id");
+            }
+
+            return advertisement;
+        }
+
+        public List<Advertisement> GetAdvertisementsList()
+        {
+            var list = this.context.Advertisements.ToList();
+            
+            return list;
+        }
+
+        public Advertisement UpdateAdvertisement(int id, Advertisement newAdvertisement)
+        {
+            var advertisement = this.context.Advertisements.FirstOrDefault(a => a.Id == id);
+            if (advertisement == null)
+            {
+                throw new ArgumentException("Invalid advertisement id.", "id");
+            }
+
+            advertisement.Name = newAdvertisement.Name;
+            advertisement.Description = newAdvertisement.Description;
+            advertisement.Active = newAdvertisement.Active;
+            advertisement.JobPosition = newAdvertisement.JobPosition;
+            advertisement.UpdatedAt = DateTime.UtcNow;
+
+            this.context.SaveChanges();
+
+            return advertisement;
+        }
+    }
+}
